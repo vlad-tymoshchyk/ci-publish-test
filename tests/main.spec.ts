f39595634d84052fc16b2422af1caea53f4f683f
Vlad Tymoshchyk <vtymoshchyk@gmail.com>
2023-11-08 21:00:47 +0200

    fix: make snapshot

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,5 +1,10 @@
+import { Selector } from 'testcafe';
+import { takeSnapshot } from 'testcafe-blink-diff';
+
 fixture`Main fixture`;
 
 test.page('http://localhost:3000')('main', async (t) => {
-  await t.takeScreenshot();
+  await t.expect(Selector('.main').visible).ok();
+
+  await takeSnapshot(t);
 });
